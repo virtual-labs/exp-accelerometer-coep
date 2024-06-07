3986756bce9a23bea12e561a5a55eccf170a1f0d
vleadadmin <admin@vlabs.ac.in>
2024-06-07 06:00:40 +0000

    https://virtual-labs.github.io/virtual-labs/exp-accelerometer-coep click on the link to test your code.

---
+++ simulation/src/questionJson.js
@@ -0,0 +1,124 @@
+
+
+QuestionsJSON = {
+		"data": {
+			"SEC": [{
+
+				"QUES": [{
+					
+  
+					"QC": "Which of the following instrument is useful only for low frequency applications?",
+					"ANS": [{
+						"ANSID": false,
+						"content": "LVDT Accelerometers"
+					}, {
+						"ANSID": false,
+						"content": "Piezo-Electric Accelerometers"
+					}, {
+						"ANSID": true,
+						"content": "Potentiometric Accelerometers"
+					},{
+						"ANSID": false,
+						"content": "Digital Accelerometers"
+					}]
+				}]
+			}, {
+
+				"QUES": [{
+					
+					"QC": "Seismic transducers are also known as ",
+					"ANS": [{
+						"ANSID": false,
+						"content": "Seismic displacement"
+					}, {
+						"ANSID": true,
+						"content": "Seismic accelerometer"
+					}, {
+						"ANSID": false,
+						"content": "Seismic velocity"
+					}, {
+						"ANSID": false,
+						"content": "Seismic acceleration"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "When accelerometers operate in the displacement mode the ratio of forcing frequency to natural frequency should be",
+					"ANS": [{
+						"ANSID": false,
+						"content": "above 200"
+					}, {
+						"ANSID": false,
+						"content": "below 2"
+					}, {
+						"ANSID": false,
+						"content": "below 1"
+					}, {
+						"ANSID": true,
+						"content": "above 2"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "When seismic transducers are used in the acceleration made they should be designed with ",
+					"ANS": [{
+						"ANSID": true,
+						"content": "stiff springs and small mass"
+					}, {
+						"ANSID": false,
+						"content": "heavy mass and weak springs"
+					}, {
+						"ANSID": false,
+						"content": "weak springs and small mass"
+					}, {
+						"ANSID": false,
+						"content": "heavy mass and small mass"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "Piezoelectric accelerometers meters:",
+					"ANS": [{
+						"ANSID": false,
+						"content": "should not be used for high frequencies above 100 Hz"
+					}, {
+						"ANSID": false,
+						"content": "should use a monitoring source of low input impedance"
+					}, {
+						"ANSID": true,
+						"content": "should be used for low frequencies"
+					}, {
+						"ANSID": false,
+						"content": "have a low natural frequency"
+					}]
+					
+				}]
+			}
+			]
+		}
+
+
+	}
+
+
+
+var ANSWERJSON = {
+			
+				"0" : "All the points mentioned",
+				"1" : "Set the standard output for the required limits of process parameter",
+				"2" : "Set majority of the points of the transmitter output in the operating region",
+				"3" : "Pump discharge flow and Outlet flow",
+				"4" : "Nothing is required to be changed",
+				"5" : "Nil"
+				
+			
+}
